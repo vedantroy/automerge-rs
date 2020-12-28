d78c3b3520ed8f149dc3e3d9a0b6386fb1500319
Alex Good <alex@memoryandthought.me>
2020-12-28 16:16:00 +0000

    Move StateTreeChange into it's own module

--- automerge-frontend/src/state_tree/mod.rs
+++ automerge-frontend/src/state_tree/mod.rs
@@ -8,12 +8,14 @@ use std::convert::TryInto;
 mod focus;
 mod multivalue;
 mod resolved_path;
+mod state_tree_change;
 use multivalue::{MultiChar, MultiValue};
 pub use resolved_path::ResolvedPath;
 use resolved_path::{
     ResolvedChar, ResolvedCounter, ResolvedList, ResolvedMap, ResolvedPrimitive, ResolvedRoot,
     ResolvedTable, ResolvedText,
 };
+use state_tree_change::StateTreeChange;
 
 /// Represents the result of running a local operation (i.e one that happens within the frontend
 /// before any interaction with a backend).
@@ -244,82 +246,6 @@ impl StateTree {
             props: self.root_map.clone(),
         }))
         .value()
-    }
-}
-
-/// Represents a change to the state tree. This is used to represent values which have changed
-/// (the type T usually ends up being either a MultiValue or a StateTreeComposite) along with
-/// changes that need to be made to indexes maintained by the state tree (the object id -> object
-/// value index for example).
-#[derive(Clone)]
-pub struct StateTreeChange<T> {
-    value: T,
-    index_updates: Option<im::HashMap<amp::ObjectID, StateTreeComposite>>,
-}
-
-impl<T> StateTreeChange<T>
-where
-    T: Clone,
-{
-    fn pure(value: T) -> StateTreeChange<T> {
-        StateTreeChange {
-            value,
-            index_updates: None,
-        }
-    }
-
-    fn value(&self) -> &T {
-        &self.value
-    }
-
-    fn index_updates(&self) -> Option<&im::HashMap<amp::ObjectID, StateTreeComposite>> {
-        self.index_updates.as_ref()
-    }
-
-    fn map<F, G>(self, f: F) -> StateTreeChange<G>
-    where
-        F: Fn(T) -> G,
-    {
-        StateTreeChange {
-            value: f(self.value),
-            index_updates: self.index_updates,
-        }
-    }
-
-    fn and_then<F, G>(self, f: F) -> StateTreeChange<G>
-    where
-        F: Fn(T) -> StateTreeChange<G>,
-    {
-        let diff = f(self.value.clone());
-        let result = self.with_updates(diff.index_updates.clone());
-        StateTreeChange {
-            value: diff.value,
-            index_updates: result.index_updates,
-        }
-    }
-
-    fn with_updates(
-        self,
-        updates: Option<im::HashMap<amp::ObjectID, StateTreeComposite>>,
-    ) -> StateTreeChange<T> {
-        match (updates, self.index_updates) {
-            (Some(updates), Some(existing_updates)) => StateTreeChange {
-                value: self.value,
-                index_updates: Some(updates.union(existing_updates)),
-            },
-            (Some(updates), None) => StateTreeChange {
-                value: self.value,
-                index_updates: Some(updates),
-            },
-            (None, Some(existing_updates)) => StateTreeChange {
-                value: self.value,
-                index_updates: Some(existing_updates),
-            },
-            (None, None) => StateTreeChange {
-                value: self.value,
-                index_updates: None,
-            },
-        }
     }
 }
 
